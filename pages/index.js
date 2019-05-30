c2b9198ad01531f32d039b2d3bdf2c9b2186c7d7
trungtv <tranviettrung92@gmail.com>
2019-05-30 14:30:35 +0700

    add pretty url

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = props => (
+    <li>
+        <Link as={`/p/${props.id}`} href={`post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
 
 export default function Index() {
     return (
         <Layout>
-            <p>Hello World</p>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink id="hello-world" title="Hello World" />
+                <PostLink id="learn-nextjs" title="Next.js" />
+                <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+            </ul>
         </Layout>
     );
 }
